64169ab1e4c9d843691ed604c33b1e9084287178
GuilleOk <fernandez.martinez1403@gmail.com>
2024-12-29 13:40:25 -0500

    cambios en los tamaños de las letras

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -10,7 +10,7 @@ const MainPage = () => {
         </div>
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
           <img src="images/technologys/codigo.png" alt="tag" style={{filter: 'invert(100%)', height: '2rem'}} />
-          <h1 style={{ textAlign: 'center', fontSize: '2vw' }}>Guillermo Alfredo Fernández Martínez</h1>
+          <h1 style={{ textAlign: 'center', fontSize: '1.45vw' }}>Guillermo Alfredo Fernández Martínez</h1>
         </div>
         <p style={{textAlign: 'center'}}>Soy Ingeniero en Telecomunicaciones y Electrónica graduado de la Universidad de Oriente de Cuba y me apasiona el desarrollo web.</p>
       </header>
