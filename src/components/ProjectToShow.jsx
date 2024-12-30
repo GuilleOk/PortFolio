003b3d4dd8cf405986099742503b6e639620297f
GuilleOk <fernandez.martinez1403@gmail.com>
2024-12-29 20:07:09 -0500

    otra prueba

--- src/components/ProjectToShow.jsx
+++ src/components/ProjectToShow.jsx
@@ -44,6 +44,48 @@ const ProjectToShow = ({name, technologys, description, image, url, urlCode}) =>
             </div>
           </div>
       </div>
+      <div className="d-flex d-lg-none flex-column justify-content-center projectToShowSM my-3">
+          <div className="col-12">
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="d-flex justify-content-start align-items-center gap-3">
+               <div>
+                  <img src="images/arrow.png" alt="arrow" style={{ filter: 'invert(100%)', height: '1rem' }} />
+               </div>
+                <div>
+                  <h2 style={{textAlign: 'center', fontSize: '1rem', color: 'rgb(159, 248, 171)'}} className="my-0">{name}</h2>
+                </div>
+              </div>
+              <a className="btn btn-something" href={urlCode} target="_blank">
+                <div className="d-flex gap-1 h-auto p-0 m-0">
+                  <p className="m-0"><strong>Código</strong></p>
+                  <img src="images/link.png" alt="arrow" style={{ height: '1rem' }} />
+                </div>              
+              </a>
+            </div>
+            <p style={{margin: '1.5rem', textAlign: 'justify'}}>{description}</p>
+            <div className="d-flex gap-2 align-items-center my-3 mx-5 px-2 technologysContainer">
+              {technologys.map(({ tech, techPhoto }) => {
+                return (
+                  <div key={`${name}${tech}`} className="d-flex align-items-center gap-2 mx-2 technologys">
+                    <img src={techPhoto} alt={tech} style={{ height: '2rem'}} />
+                    {tech}
+                    {/* <p>{tech}</p> */}
+                  </div>
+                )
+              })}
+            </div>
+            <div className="container photoProjectAllContainer">
+              <div className="arrowProjectGOContainer">
+                <a href={url} target="_blank">
+                  <img src="images/flecha-adelante2.png" alt="go" className="arrowProjectGO" />
+                </a>
+              </div>
+              <div>
+                <img src={image} alt="project image" className="imageProject" />
+              </div>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
