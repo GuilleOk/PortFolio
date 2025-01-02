9c5a6bd757c6de4d32b005bffce48220f9087b99
GuilleOk <fernandez.martinez1403@gmail.com>
2025-01-02 12:03:38 -0500

    prueba de mis estudios

--- src/components/Study.jsx
+++ src/components/Study.jsx
@@ -2,7 +2,7 @@
 const Study = ({type, name, finished, academyInstitution}) => {
   return (
     <div className="studyContainer my-4">
-      <div className="d-flex  align-items-center mb-1">
+      <div className="d-none d-lg-flex  align-items-center mb-1">
         <div>
           <img src="images/arrow.png" alt="arrow" style={{ filter: 'invert(100%)', height: '2rem' }} />
         </div>
@@ -12,9 +12,28 @@ const Study = ({type, name, finished, academyInstitution}) => {
           </h2>
         </div>
       </div>
-      <div className="curso">
+      <div className="d-none d-lg-block curso">
         <h4 style={{marginLeft: '2rem', marginBottom: '.7rem'}}>Impartido por <span style={{ color: 'rgb(159, 248, 171)' }}>{academyInstitution}</span></h4>
         <div className="form-check" style={{marginLeft: '2rem'}}>
+          <input className="form-check-input" type="checkbox" value="" readOnly id="flexCheckChecked" checked={finished} />
+          <label className="form-check-label" htmlFor="flexCheckChecked">
+            Terminado
+          </label>            
+        </div> 
+      </div>
+      <div className="d-flex d-lg-none align-items-center mb-1">
+        <div>
+          <img src="images/arrow.png" alt="arrow" style={{ filter: 'invert(100%)', height: '1rem' }} />
+        </div>
+        <div>
+          <h2 style={{ textAlign: 'center', fontSize: '1.2rem', color: 'rgb(159, 248, 171)' }} className="my-0">
+            {type} {name}
+          </h2>
+        </div>
+      </div>
+      <div className="curso d-lg-none">
+        <h4 style={{marginLeft: '1rem', marginBottom: '.7rem', fontSize: '.7rem'}}>Impartido por <span style={{ color: 'rgb(159, 248, 171)' }}>{academyInstitution}</span></h4>
+        <div className="form-check" style={{marginLeft: '1rem'}}>
           <input className="form-check-input" type="checkbox" value="" readOnly id="flexCheckChecked" checked={finished} />
           <label className="form-check-label" htmlFor="flexCheckChecked">
             Terminado
