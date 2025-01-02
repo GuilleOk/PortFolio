36885a8cee1897e8282f28f1c48802f7d144b2c5
GuilleOk <fernandez.martinez1403@gmail.com>
2025-01-02 00:52:00 -0500

    pequeño arreglo en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div style={{ background: 'rgb(2, 48, 21)' }}>
       <footer className="d-none d-lg-block pt-3 pb-1">
-        <ul className="nav justify-content-center gap-3 align-items-center pb-2 mx-5">
+        <ul className="nav justify-content-center gap-4 align-items-center pb-2 mx-5">
           <li className="nav-item">
             <NavLink to="/projects" className="nav-link px-2 textNavbar">
               Mis Proyectos
@@ -38,7 +38,7 @@ const Footer = () => {
       </footer>
       <footer className="d-block d-lg-none pt-3 pb-1">
         <div className='pb-2'>
-        <ul className="nav justify-content-between gap-1 align-items-center mx-2">
+        <ul className="nav justify-content-between align-items-center mx-2">
           <li className="nav-item">
             <NavLink to="/projects" className="nav-link px-2 textNavbarSM">
               Mis Proyectos
