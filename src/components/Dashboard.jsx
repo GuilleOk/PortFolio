7d5b2b129a30117cf6471159fa39d77abec8725b
GuilleOk <fernandez.martinez1403@gmail.com>
2025-02-27 11:55:53 +0100

    ya se contrae el navbar al hacer clic en un enlace

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,16 @@
 import { NavLink } from "react-router-dom"
+import { Collapse } from "bootstrap"
+import { useRef } from "react"
 
 const Dashboard = () => {
+  const navbarCollapse = useRef(null)
+
+  const handleCollapse = () => {
+    if (navbarCollapse.current && window.innerWidth < 992) { // 992px es el breakpoint LG de Bootstrap
+      const bsCollapse = new Collapse(navbarCollapse.current, { toggle: false })
+      bsCollapse.hide()
+    }
+  }
   return (
     <div style={{position: 'fixed', top: '0', zIndex: '10000', maxWidth: '100vw'}}>
       <nav className="navbar navbar-expand-lg backgroundNavbar" data-bs-theme='dark'>
@@ -17,24 +27,24 @@ const Dashboard = () => {
             </a>
         </div>
         <div style={{width: 'auto'}}>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <div ref={navbarCollapse} className="collapse navbar-collapse" id="navbarTogglerDemo03">
           <ul className="navbar-nav mb-2 mb-lg-0">
-            <li className="nav-item item">
+            <li className="nav-item item" onClick={handleCollapse}>
               <NavLink to="/" className={({isActive})=> isActive ? 'nav-link activeLink': 'nav-link textNavbar'}>Inicio</NavLink>
             </li>
-            <li className="nav-item item">
+            <li className="nav-item item" onClick={handleCollapse}>
               <NavLink to="/projects" className={({isActive})=> isActive ? 'nav-link activeLink': 'nav-link textNavbar'}>Mis Proyectos</NavLink>
             </li>
-            <li className="nav-item item">
+            <li className="nav-item item" onClick={handleCollapse}>
               <NavLink to="/my_learning_path" className={({isActive})=> isActive ? 'nav-link activeLink': 'nav-link textNavbar'}>Mis Estudios</NavLink>
             </li>
-            <li className="nav-item item">
+            <li className="nav-item item" onClick={handleCollapse}>
               <NavLink to="/about" className={({isActive})=> isActive ? 'nav-link activeLink': 'nav-link textNavbar'}>Acerca de mí</NavLink>
             </li>
-            <li className="nav-item item">
+            <li className="nav-item item" onClick={handleCollapse}>
               <a href="/CVGuillermoAlfredoFernandezMartinez25-02-2025.pdf" rel='noreferrer' target="_blank" download className='nav-link textNavbar'>CV</a>
             </li>
-            <li className="nav-item item mx-lg-2">
+            <li className="nav-item item mx-lg-2" onClick={handleCollapse}>
               <div style={{padding: '.5rem 0', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                 <a href="https://www.linkedin.com/in/guillermo-alfredo-fernandez-martinez-a7a59128a" target="_blank"><img src="images/redes_sociales/linkedin.png" alt="linkedin" className="socialMediaPhoto" /></a>
               </div>
